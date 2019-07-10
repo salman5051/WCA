31a41dc12f3e57277cd897483974287fd7628917
Salman Shaik <SShaik@CALVAUTO.LOCAL>
2019-07-10 17:14:06 -0400

    post try

--- src/app/remote-diagnosis-service.service.ts
+++ src/app/remote-diagnosis-service.service.ts
@@ -5,49 +5,51 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class RemoteDiagnosisServiceService {
-
+	dates;
   constructor(private http: HttpClient) { }
   getRemoteDiagnosisServiceData(){
-    var data = [{"time":"2019-07-01 13:55:29","Data":{"Robot":{"Status":"OK","Temperature":70},
+	  
+    var data = [{"time":"07/03/2019, 11:49:36 AM","Data":{"Robot":{"Status":"OK","Temperature":70},
 	"Gantry":{"Status":"OK","Position":1000,"Other1":"aaa","Other2":"bbb"},
 	"Freezer":{"Status":"OK","Position":1000,"Other1":"aaa","Other2":"bbb"}}},
-{"time":"2019-07-01 13:55:29","Data":{"Robot":{"Status":"OK","Temperature":68},
+{"time":"07/03/2019, 11:49:36 AM","Data":{"Robot":{"Status":"OK","Temperature":68},
 	"Gantry":{"Status":"Error","Position":1200,"Other1":"bbb","Other2":"ccc"},
 	"Freezer":{"Status":"OK","Position":1000,"Other1":"aaa","Other2":"bbb"}}},
-	{"time":"2019-07-01 13:55:29","Data":{"Robot":{"Status":"OK","Temperature":68},
+	{"time":"07/03/2019, 11:49:36 AM","Data":{"Robot":{"Status":"OK","Temperature":68},
 	"Gantry":{"Status":"Error","Position":1200,"Other1":"bbb","Other2":"ccc"},
 	"Freezer":{"Status":"OK","Position":1000,"Other1":"aaa","Other2":"bbb"}}},		
-	{"time":"2019-07-01 13:55:29","Data":{"Robot":{"Status":"OK","Temperature":68},
+	{"time":"07/03/2019, 11:49:36 AM","Data":{"Robot":{"Status":"OK","Temperature":68},
 	"Gantry":{"Status":"Error","Position":1200,"Other1":"bbb","Other2":"ccc"},
 	"Freezer":{"Status":"OK","Position":1000,"Other1":"aaa","Other2":"bbb"}}},		
-	{"time":"2019-07-01 13:55:29","Data":{"Robot":{"Status":"OK","Temperature":68},
+	{"time":"07/03/2019, 11:49:36 AM","Data":{"Robot":{"Status":"OK","Temperature":68},
 	"Gantry":{"Status":"Error","Position":1200,"Other1":"bbb","Other2":"ccc"},
 	"Freezer":{"Status":"OK","Position":1000,"Other1":"aaa","Other2":"bbb"}}},		
-	{"time":"2019-07-01 13:55:29","Data":{"Robot":{"Status":"OK","Temperature":68},
+	{"time":"07/03/2019, 11:49:36 AM","Data":{"Robot":{"Status":"OK","Temperature":68},
 	"Gantry":{"Status":"Error","Position":1200,"Other1":"bbb","Other2":"ccc"},
 	"Freezer":{"Status":"OK","Position":1000,"Other1":"aaa","Other2":"bbb"}}},		
-	{"time":"2019-07-01 13:55:29","Data":{"Robot":{"Status":"OK","Temperature":68},
+	{"time":"07/03/2019, 11:49:36 AM","Data":{"Robot":{"Status":"OK","Temperature":68},
 	"Gantry":{"Status":"Error","Position":1200,"Other1":"bbb","Other2":"ccc"},
 	"Freezer":{"Status":"OK","Position":1000,"Other1":"aaa","Other2":"bbb"}}},		
-	{"time":"2019-07-01 13:55:29","Data":{"Robot":{"Status":"OK","Temperature":68},
+	{"time":"07/03/2019, 11:49:36 AM","Data":{"Robot":{"Status":"OK","Temperature":68},
 	"Gantry":{"Status":"Error","Position":1200,"Other1":"bbb","Other2":"ccc"},
 	"Freezer":{"Status":"OK","Position":1000,"Other1":"aaa","Other2":"bbb"}}},
 	
-	{"time":"2019-07-01 13:55:29","Data":{"Robot":{"Status":"OK","Temperature":68},
+	{"time":"07/03/2019, 11:49:36 AM","Data":{"Robot":{"Status":"OK","Temperature":68},
 	"Gantry":{"Status":"Error","Position":1200,"Other1":"bbb","Other2":"ccc"},
 	"Freezer":{"Status":"OK","Position":1000,"Other1":"aaa","Other2":"bbb"}}},		
-	{"time":"2019-07-01 13:55:29","Data":{"Robot":{"Status":"OK","Temperature":68},
+	{"time":"07/03/2019, 11:49:36 AM","Data":{"Robot":{"Status":"OK","Temperature":68},
 	"Gantry":{"Status":"Error","Position":1200,"Other1":"bbb","Other2":"ccc"},
 	"Freezer":{"Status":"OK","Position":1000,"Other1":"aaa","Other2":"bbb"}}},		
-	{"time":"2019-07-01 13:55:29","Data":{"Robot":{"Status":"OK","Temperature":68},
+	{"time":"07/03/2019, 11:49:36 AM","Data":{"Robot":{"Status":"OK","Temperature":68},
 	"Gantry":{"Status":"Error","Position":1200,"Other1":"bbb","Other2":"ccc"},
 	"Freezer":{"Status":"OK","Position":1000,"Other1":"aaa","Other2":"bbb"}}},		
-	{"time":"2019-07-01 13:55:29","Data":{"Robot":{"Status":"OK","Temperature":68},
+	{"time":"07/03/2019, 11:49:36 AM","Data":{"Robot":{"Status":"OK","Temperature":68},
 	"Gantry":{"Status":"Error","Position":1200,"Other1":"bbb","Other2":"ccc"},
 	"Freezer":{"Status":"OK","Position":1000,"Other1":"aaa","Other2":"bbb"}}},		
-	{"time":"2019-07-01 13:55:29","Data":{"Robot":{"Status":"OK","Temperature":68},
+	{"time":"07/03/2019, 11:49:36 AM","Data":{"Robot":{"Status":"OK","Temperature":68},
 	"Gantry":{"Status":"Error","Position":1200,"Other1":"bbb","Other2":"ccc"},
 	"Freezer":{"Status":"OK","Position":1000,"Other1":"aaa","Other2":"bbb"}}}];
-    return data;
+
+	return data;
   }
 }
--- src/app/remote-diagnosis/remote-diagnosis.component.ts
+++ src/app/remote-diagnosis/remote-diagnosis.component.ts
@@ -13,31 +13,46 @@ import { dayOfYearFromWeeks } from 'ngx-bootstrap/chronos/units/week-calendar-ut
   styleUrls: ['./remote-diagnosis.component.css']
 })
 export class RemoteDiagnosisComponent implements OnInit {
+
   form: FormGroup
   public RemoteDiagnosis: any = [];
   _object= Object;
+  
   constructor(private formBuilder: FormBuilder, private service:RemoteDiagnosisServiceService) { }
 
   isDataAvailable =false;
   maxDate: Date = new Date();
-  minDate: Date = new Date();  
+  minDate: Date = new Date();
   startDate: Date = new Date();
+  dateStart: Date;
+  dateEnd: Date;
   
   private bsRangeValue: any = [new Date(this.startDate.setDate(-20)), new Date()];
 
-
+  data: Date;
 
   ngOnInit() {
     
   }
-  getData(){
+  getData(value: Date){
+   
+    this.data= value;
+    this.dateStart = this.data[1];
+    console.log(this.dateStart)
+    console.log(this.data);
     this.getRemoteDiagnosisData();
+    
+
 
     this.isDataAvailable= true;
   }
 
    getRemoteDiagnosisData(){
+  
     this.RemoteDiagnosis = this.service.getRemoteDiagnosisServiceData();
+    
+    console.log(this.RemoteDiagnosis);
+ 
   }
 
   
